3c344076ab9e17fa8b64dea64d462e37aa82b2e3
pratikraj12341620 <pratikee620@gmail.com>
2026-08-17 18:54:32 +0530

    feat(analysis): expose public analysis API

---
+++ apps/api/src/analysis/index.ts
@@ -0,0 +1,18 @@
+export { analyzeBlastRadius } from "./core/blast-radius.js";
+export { classifyExposure } from "./core/exposure-ladder.js";
+
+export type {
+  AnalysisWarning,
+  AnalysisWarningCode,
+  AppliedBlastRadiusLimits,
+  BlastRadiusOptions,
+  BlastRadiusPath,
+  BlastRadiusResult,
+  BlastRadiusServiceCandidate,
+  DependencyHop,
+  ExposureAssessment,
+  ExposureEvidenceSignals,
+  ExposureStage,
+  ReadonlyGraphReader,
+  SecurityConclusion,
+} from "./core/analysis-types.js";
